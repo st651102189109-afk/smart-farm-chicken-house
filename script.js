2f7f802f0eda6a81a7c7e6c1b26b3f5f99641535
st651102189109-afk <st651102189109@pcru.ac.th>
2026-04-29 01:22:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,14 @@
-// READ SENSOR
-db.ref("sensor").on("value", (snap) => {
-  const data = snap.val();
-  document.getElementById("temp").innerText = data.temp;
-  document.getElementById("hum").innerText = data.hum;
+// SENSOR
+db.ref("farm").on("value", (snap) => {
+  const d = snap.val();
+
+  document.getElementById("temp").innerText = d.temp;
+  document.getElementById("hum").innerText = d.humidity;
+  document.getElementById("water").innerText = d.water;
+  document.getElementById("feed").innerText = d.feed;
 });
 
-// READ MODE
-db.ref("mode").on("value", (snap) => {
-  document.getElementById("mode").innerText = snap.val();
-});
-
-// SET MODE
-function setMode(mode) {
-  db.ref("mode").set(mode);
-}
-
-// CONTROL FAN
-function toggleFan(state) {
-  db.ref("control/fan").set(state);
-}
-
-// CONTROL LIGHT
-function toggleLight(state) {
-  db.ref("control/light").set(state);
+// LIGHT CONTROL
+function setLight(val){
+  db.ref("control/light").set(val);
 }
