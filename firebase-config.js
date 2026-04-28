a1070b18b7527af44372cc9d93bd8fbd320002f0
st651102189109-afk <st651102189109@pcru.ac.th>
2026-04-29 01:22:05 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,8 @@
 const firebaseConfig = {
   apiKey: "FUeTzNWVD9EUpOLPkKwaAf3sSV2TvaH2qbRrc8S6",
-  authDomain: "",
-  databaseURL: "smartfarmfishlv1-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "smartfarmfishlv1",
+  authDomain: "smartfarmfishlv1.firebaseapp.com",
+  databaseURL: "https://smartfarmfishlv1-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "smartfarmfishlv1"
 };
 
 firebase.initializeApp(firebaseConfig);
