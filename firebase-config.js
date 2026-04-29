6cd09c36ecf9df4666572358cf1dc83e0828b721
st651102189109-afk <st651102189109@pcru.ac.th>
2026-04-29 16:50:16 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,8 +2,5 @@ const firebaseConfig = {
   apiKey: "Iewz0nqBvGXqpdNASY6aDlory3McE3CkdPjofBIT",
   authDomain: "",
   databaseURL: "https://smartfarmchicken-798f2-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "smartfarmchicken-798f2"
+  projectId: "smartfarmchicken-798f2",
 };
-
-firebase.initializeApp(firebaseConfig);
-const db = firebase.database();
